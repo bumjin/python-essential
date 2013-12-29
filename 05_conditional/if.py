c1d4c945b4941257ad33e0c6104711bc1dfe3442
Beomjin Park <bumjin@gmail.com>
2013-12-29 19:59:51 +0900

    using in where if statement

--- 05_conditional/if.py
+++ 05_conditional/if.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 
-name = raw_input('Plaese type in your name: ')
+language = raw_input('Plaese type in your name: ')
 
-if len(name) < 5:
-	print	"Your name has fewer than 5 chracters"
-elif len(name) == 5:
-	print "Your name hs exactly 5 chracters"
-	if name == 'Jesse':
-		print "Hey Jesse!"
-else:
-	print	"Your name has greater than 5 chracters"
+if language in ['C++', 'Python', 'Java']:
+	print language, "rocks"
+
