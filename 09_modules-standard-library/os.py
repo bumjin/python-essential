10e5ed5feb9009126717fc68ea09ad9b50fb3bfc
Beomjin Park <bumjin@gmail.com>
2013-12-31 20:00:34 +0900

    using import

---
+++ 09_modules-standard-library/os.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import os
+os.chdir('/Users/bumjin/Desktop');
+print os.getcwd()
+os.makedirs('dir created from python!')
+#os.system("growlnotify -m 'growling from python!'")
